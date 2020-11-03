0c875c88d3db61b739379e95acf00da48ff278f6
SinghHarmanjit <harmanjit.x.singh@gmail.com>
2020-11-03 20:48:14 +1100

    Integrated with api

--- src/main/java/com/technochorus/agile/service/AgileService.java
+++ src/main/java/com/technochorus/agile/service/AgileService.java
@@ -8,13 +8,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AgileService implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
-
     private Map<String, String> headers = new HashMap<>();
+    // NOTE: CORS support only * or a specific origin
+    private String[] ALLOWED_ORIGINS = new String[] {
+            "https://technochorus.com",
+            "https://www.technochorus.com"};
 
     public AgileService() {
         headers.put("Content-Type", "application/json");
-        headers.put("Access-Control-Allow-Origin", "technochorus.com");
         headers.put("Access-Control-Allow-Credentials", "true");
+        headers.put("Access-Control-Allow-Origin", ALLOWED_ORIGINS[0]);
     }
 
     @Override
